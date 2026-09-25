0f63ea3fbd715b5dc218c6de3e9dddccc424dbe1
TrongDang2608 <zeusgamez2608@gmail.com>
2026-09-25 08:58:30 +0700

    complete section 7

--- section_6_7/books/src/App.js
+++ section_6_7/books/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
@@ -6,32 +7,49 @@ import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return { ...book, title: newTitle };
-            }
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    };
 
-            return book;
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
         });
 
+        console.log(response);
+
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, ...response.data };
+            }
+            return book;
+        });
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
-
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title: title,
+        });
+
         const updatedBooks = [
             ...books,
-            { id: Math.round(Math.random() * 9999), title }
+            response.data
         ];
-
         setBooks(updatedBooks);
     };
 
